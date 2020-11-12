a24b2ea523cdae17936f88c6f954665b788585f1
Jimmy Blaszkiewicz <10551532+jimmyblaszkiewicz@users.noreply.github.com>
2020-11-12 16:26:53 -0500

    add colors to syntax
    Why was this just all white before? We may never know

--- Assets/SyntaxTheme.cs
+++ Assets/SyntaxTheme.cs
@@ -3,15 +3,15 @@
 
 public class SyntaxTheme : ScriptableObject
 {
-    public Color comment = Color.white;
-    public Color loopKeyword = Color.white;
-    public Color conditionKeyword = Color.white;
-    public Color mathOperator = Color.white;
+    public Color comment = Color.green;
+    public Color loopKeyword = Color.blue;
+    public Color conditionKeyword = Color.blue;
+    public Color mathOperator = Color.red;
     public Color value = Color.white;
     public Color parentheses = Color.white;
     public Color curlyBrackets = Color.white;
-    public Color comparisonOperator = Color.white;
+    public Color comparisonOperator = Color.red;
     public Color assignment = Color.white;
-    public Color variable = Color.white;
+    public Color variable = Color.cyan;
 
 }
